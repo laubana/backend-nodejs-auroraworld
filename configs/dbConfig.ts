9c63c619efc5c5404a70a4ce516dd4e02c33c4ef
yban <laubana@gmail.com>
2025-02-16 22:55:36 -0800

    fix bugs

--- configs/dbConfig.ts
+++ configs/dbConfig.ts
@@ -1,7 +1,7 @@
 import Database from "better-sqlite3";
 import path from "path";
 
-const db = new Database(path.join(__dirname, "/tmp", "data.db"));
+const db = new Database(path.join("/", "tmp", "data.db"));
 
 const initiate = () => {
   try {
